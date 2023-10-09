309348773d547e0f69a744c153da2bafa47c66e3
Arnav K <arnavkaushal09@gmail.com>
2023-10-09 23:57:27 +0530

    FIXED ISSUES (#7)

---
+++ src/components/Avatar/Page/Share.svelte
@@ -0,0 +1,19 @@
+<button
+    title="Share This Avatar"
+    style="z-index: 1;"
+    class="share_avatar_btn w-12 md:w-16 md:h-16 h-12 px-4 py-4 fixed md:bottom-10 bottom-7 md:right-8 right-6 bg-gradient-to-br from-primary to-primary-dark hover:to-primary hover:from-primary-dark rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:drop-shadow-2xl hover:shadow-2xl hover:scale-105 transition ease-in-out duration-300"
+>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="2.5"
+        class="w-14 h-14 md:m-0.5 stroke-white"
+    >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+        />
+    </svg>
+</button>
