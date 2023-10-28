2a5aea043822c0d1b429e2a1020a9406e458e8f9
Arnav K <arnavkaushal09@gmail.com>
2023-10-28 18:06:57 +0530

    Update Tag.svelte

--- src/components/UI/Tag.svelte
+++ src/components/UI/Tag.svelte
@@ -9,7 +9,7 @@
     if (!window.document) return;
     window.document.location.href = `//${
       window.document.location.host
-    }/explore?q=tag:${tag.toLowerCase()}#explore`;
+    }/explore?q=tag:${tag.toLowerCase()}`;
   }}
   class={`line-clamp-1 isTag rounded-full select-none capitalize hover:brightness-125 transition bg-primary-dark/10 px-2.5 py-0.5 text-xs md:text-sm text-primary`}
 >
